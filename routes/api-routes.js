f4becaf7f68e4f5fc573b6e031f6ba5e368d555a
Alexander Himchak <alexhimchak@alexanders-mbp.attlocal.net>
2017-09-29 10:12:38 -0700

    Finish

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,7 +1,54 @@
 var db = require("../models/index.js");
 var path = require('path');
+// const setupPassport = require('./setupPassport.js');
+// const twiliovoice = require('./contactmethods/twiliovoice.js');
+const twiliosms = require('./twiliosms.js');
+// const sendgrid = require('./contactmethods/sendgrid.js')
+
 
 module.exports = function(app) {
+
+ // let isAuthenticated = function(req, res, next){
+  //   if(req.isAuthenticated())
+  //     return next()
+  //   req.flash('error', 'Need to be logged in')
+  //   res.redirect('/')
+  // } 
+
+  // app.post('/signup', signupController.signup);
+
+  // app.post('/login', function(req, res, next) {
+  //     passport.authenticate('local', {failureFlash:true}, function(err, user, info) {
+  //       console.log(user)
+  //       let userid = user.id;
+  //      if (err) { return next(err); }
+  //      if (!user) { return res.redirect('/'); }
+  //     req.logIn(user, function(err) {
+  //       if (err) { return next(err); }
+  //       console.log(req.user.username);
+  //      return res.redirect('/user/affiliate?id=' + userid);
+  //    });
+  //   })(req, res, next);
+  // });
+
+  // app.get('/logout', function(req,res){
+  //   req.logout();
+  //   res.redirect('/');
+  // })
+
+
+  app.post('/contact/twiliosms', function(req,res){
+    twiliosms(req.body.phone, "You have been contacted via Solarize");
+  });
+
+  // app.post('./contact/twiliovoice', function(req,res){
+  //   twiliovoice(req.body.phone);
+  // });
+
+  app.post('./contact/email', function(req,res){
+    sendgrid(req.body.email, "From Solarize", "You have been contacted via Solarize");
+  });
+
   app.get("/api/user", function(req, res) {
   db.User.findAll({}).then(function(dbUser) {
       res.json(dbUser);
